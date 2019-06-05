f207b79a3220755d025928c670db366cc307118e
BStarr <beccastarr@10-16-101-53.dynapool.wireless.nyu.edu>
2019-06-05 19:02:39 -0400

    if/else update

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
 print("Rock, Paper, Scissors, Shoot!")
+
+# CAPTURE INPUTS
+
+user_choice = input("Please choose one of the following options: 'rock', 'paper' or 'scissors'(without the quotes):")
+print("-------------")
+print("USER CHOICE:", user_choice)
+
+# VALIDATE INPUTS
+
+if ________:
+    print("VALID")
+else:
+    print("NOT VALID")
+
+# GENERATE COMPUTER SELECTION
+
+# DETERMINE THE WINNER
+
+# DISPLAY FINAL OUTPUT / OUTCOMES
+
