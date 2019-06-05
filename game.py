bc902cd282646a2b263924c0612770e75a060919
BStarr <beccastarr@10-16-101-53.dynapool.wireless.nyu.edu>
2019-06-05 19:07:48 -0400

    something wrong

--- game.py
+++ game.py
@@ -2,14 +2,14 @@ print("Rock, Paper, Scissors, Shoot!")
 
 # CAPTURE INPUTS
 
-user_choice = input("Please choose one of the following options: 'rock', 'paper' or 'scissors'(without the quotes):")
+user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
 print("-------------")
 print("USER CHOICE:", user_choice)
 
 # VALIDATE INPUTS
 
-if ________:
-    print("VALID")
+if user_choice in ["rock", "paper", "scissors"]:
+        print("VALID")
 else:
     print("NOT VALID")
 
